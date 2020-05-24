e09f1f5d237a06de3f1e4b235bdc7758cc009f08
Kubsonowskyy <63148273+Kubsonowskyy@users.noreply.github.com>
2020-05-24 12:16:38 +0200

    main

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require(`discord.js`)
+const bot = new Discord.Client();
+const prefix = "!"
+var nazwabota = "MEE6+"
+const token = `NzE0MDU2NTI1MDM1MDc3NjUz.XspHBA.BD4qkF9rY3322xEwCpjuZBqWpTw`;
+
+bot.on(`ready`, ()=>{
+    console.log(`Bot ${nazwabota} został włączony z tokenu: ${token}`);
+    
+})
+
+bot.on("message", async message => {
+    if (message.author.bot) return;
+ 
+    if (message.content.indexOf(prefix) !== 0) return;
+    var args = message.content.slice(prefix.length).trim().split(/ +/g);
+    var command = args.shift().toLowerCase()
+});     
+bot.login(token);
